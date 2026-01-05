746d52f50b61bbab8a5bb6887f58773303fbe8fa
An, Dinh Le Binh <dinhlebinhan2005@gmail.com>
2026-01-05 10:43:35 +0700

    Delete src_agent directory

--- src_agent/turn.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Đọc file CSV
-df = pd.read_csv("/home/manh/Projects/temp/CS431/src_agent/eval_results_checkpoint_318.csv", encoding="utf-8")
-
-# Ghi ra file Excel
-df.to_excel("/home/manh/Projects/temp/CS431/data_318_1000sample.xlsx", index=False)
